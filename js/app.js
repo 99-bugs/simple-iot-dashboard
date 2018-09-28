4597d6267e0c86f0bbd70568d4dd8f6d4bef1769
Sille Van Landschoot <info@sillevl.be>
2018-09-28 13:49:07 +0200

    Refactor to Vue-Mqtt

--- js/app.js
+++ js/app.js
@@ -4,43 +4,25 @@
 
 document.addEventListener("DOMContentLoaded",function(){
 
+  const topic = 'webtechnology/hello-world/weather'
+
+  Vue.use(VueMqtt.default, 'ws://mqtt.labict.be:1884')
+
   // Create a new Vue application. 
-  const app = new Vue({
+  new Vue({
     el: '#app',         // the element on which the Vue app must be build on
     data: {             // the application contains some data
-      weather: {},      // some weather information, but we do not have any values yet
-      lastUpdate: 0
+      weather: {}       // some weather information, but we do not have any values yet
+    },
+    mounted () {
+      this.$mqtt.subscribe(topic)       // subscribe to the information topic on MQTT
+    },
+    mqtt: {
+      [topic]: function(message) {        // when a message arrives on our topic
+        let json = (new TextDecoder("utf-8").decode(message))    // we need to convert (decode) the byte information to a text string
+        let data = JSON.parse(json)       // parse the JSON format to an object
+        this.$data.weather = data         // 
+      }
     }
   });
-
-  Vue.use(vueMoment);
-
-  // MQTT client to get weather information and real-time updates
-
-  // Every client must have an unique id, lets create a random string value
-  const clientId = Math.random().toString(36).substring(20);
-
-  // create a client object that will connect to the labict MQTT broker
-  const client = new Paho.MQTT.Client('mqtt.labict.be', 1884, '', clientId);
-
-  // when an update is received, the following function will called with the information
-  client.onMessageArrived = function(message){
-    let data = JSON.parse(message.payloadString)    // parse the JSON format to an object
-    console.log(data);
-    app.$data.weather = data;                       // pass the data to the Vue app
-  }
-
-  // to connect to the MQTT broker, some extra options must be specified
-  const options = {
-    // when the connection is successful, we need to subscribe to the weather channel
-    onSuccess: function() {
-      console.log("connected !");
-      client.subscribe("webtechnology/hello-world/weather");
-    }
-  }
-
-  // everything is set, we are now ready to connect to the MQTT broker
-  client.connect(options);
-  console.log("connecting");
-  
 });
